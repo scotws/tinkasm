0e65442c2455b90b696f5a7be5151d4dde95fc1b
Scot W. Stevenson <scot.stevenson@gmail.com>
2015-10-22 15:52:20 +0200

    Created basic processing structure of individual little lines

--- tasm65816.py
+++ tasm65816.py
@@ -2,7 +2,7 @@
 # A Typist's Assembler for the 65816 in Forth 
 # Scot W. Stevenson <scot.stevenson@gmail.com>
 # First version: 24. Sep 2015
-# This version: 20. Oct 2015
+# This version: 22. Oct 2015
 
 # TODO License
 
@@ -11,8 +11,9 @@
 ### SETUP ### 
 
 import argparse
-import shlex        # https://docs.python.org/3/library/shlex.html
+# import shlex        # https://docs.python.org/3/library/shlex.html
 import sys
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', dest='source', required=True, 
@@ -20,27 +21,163 @@ parser.add_argument('-i', '--input', dest='source', required=True,
 parser.add_argument('-o', '--output', dest='output', 
         help='Binary output file', default='tasm.bin')
 parser.add_argument('-l', '--listing', dest='listing', 
-        help='Human-readable code listing', default='listing.txt')
+        help='Human-readable code listing', default='tasm.txt')
+parser.add_argument('-v', '--verbose', 
+        help='Display each step as it is happening', action='store_true')
+parser.add_argument('-d', '--dump', 
+        help='Dump content of each inbetween step', action='store_true')
 args = parser.parse_args()
 
+def verbose(s):
+    """Print information string given if --verbose flag was set. Later 
+    expand this by the option of priting to a log file instead."""
+    if args.verbose:
+        print('STEP', s)
 
-### IMPORTING SOURCE FILE ###
+def dump(l):
+    """At each assembly stage, print the complete stage as a list. Produces
+    an enormous amount of output, probably only interesting for debugging."""
+    if args.dump:
+        for line in l:
+            print(line)
 
-# import raw source file 
+### CONSTANTS ###
+
+COMMENT = ';'       # Change this for a different comment marker 
+
+
+### HELPER FUNCTIONS ###
+
+# TODO see if this should be a formal error with RAISE and all of that
+def fatal(l,s): 
+    """Abort program because of fatal error during assembly"""
+    print('FATAL ERROR in line', l, ":", s)
+    sys.exit(1) 
+
+
+### MULTI-PASS ASSEMBLER ###
+
+# The assembler works by connecting as many little steps as possible (see the 
+# Manual for details). Each step is given a title such as RAW, and all
+# requirements for that step are kept close to the actual processing. 
+
+# Note we don't process lists of code, but lists of tuples which consist of
+# the original line number and a line of code as a string. That way, we can
+# always reference the original line number at each stage. 
+
+# TODO Print banner if verbose 
+
+# --- Step RAW: Import original code and add line numbers ---
+
+# Line numbers start with 1 because this is for humans
+
 with open(args.source, "r") as f:
-    pure_source = f.read()
+    sc_raw = list(enumerate(f.readlines(),1))
 
-# clean up raw source file 
-work_source = pure_source.strip()
+verbose('RAW: Read {0} lines from {1}'.format (len(sc_raw), args.source))
+dump(sc_raw) 
 
-# Must have ORIGIN and END directives
-if 'origin' not in work_source: 
-    print ("FATAL: No 'origin' directive found in source code.") 
-    sys.exit(1) 
 
-if 'end' not in work_source: 
-    print ("FATAL: No 'end' directive found in source code.") 
-    sys.exit(1) 
+# --- Step EMPTY: Remove empty lines ---
+# TODO keep empty lines in separate list to reconstruct listing file
+
+sc_empty = []
+
+for l in sc_raw:
+    if l[1].strip():
+        sc_empty.append(l) 
+
+verbose('EMPTY: Removed {0} empty lines'.format(len(sc_raw)-len(sc_empty)))
+dump(sc_empty) 
+
+
+# --- Step COMMENTS: Remove comments that span whole lines ---
+# TODO keep comment lines in separate list to reconstruct listing file
+
+sc_comments = []
+
+for l in sc_empty:
+    if l[1].strip()[0] != COMMENT :
+        sc_comments.append(l) 
+
+verbose('COMMENTS: Removed {0} full-line comments'.format(len(sc_empty)-len(sc_comments)))
+dump(sc_comments) 
+
+
+# -- Step INLINES: Remove comments that are inline ---
+# TODO keep inline comments in a separate list to reconstruct file listing
+
+def remove_inlines(l): 
+    """Remove any inlines, defined by COMMENT char. Note we only strip the 
+    right side because we might need the whitespace on the left later."""
+    return l.split(COMMENT)[0].rstrip() 
+
+sc_inlines = []
+
+for n, l in sc_comments:
+    sc_inlines.append((n, remove_inlines(l)))
+
+verbose('INLINES: Removed inline comments') 
+dump(sc_inlines) 
+
+    
+# --- Step MACROS: Define macros ---
+# TODO code this 
+
+verbose('MACROS: Defined 0 macros (DUMMY, not coded yet)')
+# TODO add dump 
+
+
+# --- STEP EXPAND: Expand macros in source code --- 
+# TODO code this 
+
+sc_expand = sc_inlines
+
+verbose('EXPAND: Expanded 0 macros (DUMMY, not coded yet)')
+dump(sc_expand) 
+
+
+# --- Step LOWER: Convert everything to lower case ---
+
+sc_lower = [(n, l.lower()) for n, l in sc_expand] 
+verbose('LOWER: Converted all remaining code to lower case')
+dump(sc_lower) 
+
+
+# --- Step ORIGIN: Find ORIGIN directive, initialize location counter ---
+
+sc_origin = []
+LC = 0 
+
+# ORIGIN line should be in first line now 
+originline = sc_lower[0][1].strip().split() 
+
+if originline[0] != "origin":
+    l = sc_lower[0][0]
+    fatal(l, 'No ORIGIN directive found') 
+
+# TODO Currently assumes ORIGIN string is hex, process number
+LC = int(originline[1], 16) 
+sc_origin = sc_lower[1:]
+
+verbose('ORIGIN: Found ORIGIN directive, setting LC to {0}'.format(originline[1]))
+dump(sc_origin) 
+
+
+# --- Step END: Make sure we have an END directive --- 
+
+endline = sc_origin[-1][1].strip().split() 
+
+if endline[0] != "end":
+    fatal(sc_origin[0][0], 'No END directive found') 
+
+sc_end = sc_origin[:-1]
+
+verbose('END: Found END directive in last line') 
+dump(sc_end) 
+
+
+# TODO HIER HIER TODO 
 
 
 ### BASIC DATA STRUCTURES ###
@@ -158,30 +295,6 @@ def opcode_length(opc):
     return opcode_data(opc)[2]
 
 
-### PARSING STEPS ###
-
-# Pass 0 : Macros (must be defined before ORIGIN line)
-print ("Pass 0: Macros (NON-FUNCTIONAL IN PRE-ALPHA)")
-
-# Handle INCLUDE 
-
-
-# Pass 1 : Symbols, create Symbol Table and Intermediate File
-print ("Pass 1: Generating Symbol Table") 
-
-
-# Pass 1.5 : Embedded Python Code
-print ("Pass 1.5: Running embedded Python Code (NON-FUNCTIONAL IN PRE-ALPHA)") 
-
-# Creates intermediate file 
-
-# Pass 2 : Assemble binary file, create listing file
-print ("Pass 2: Assembling binary file, creating listing file")
-
-### TODO TESTING TODO ###
-
-print ("---")
-print (work_source) 
 
 
 ### END ###
